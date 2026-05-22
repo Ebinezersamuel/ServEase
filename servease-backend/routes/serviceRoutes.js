d582d92876c520be4b504065ac8fa778f388d60f
Ebinezer Samuel <162179096+Ebinezersamuel@users.noreply.github.com>
2026-05-22 14:22:15 +0530

    Create service routes

---
+++ servease-backend/routes/serviceRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { getAllServices, getServiceById, createService, updateService, deleteService } = require('../controllers/serviceController');
+const { auth } = require('../middleware/auth');
+
+router.get('/', getAllServices);
+router.get('/:id', getServiceById);
+router.post('/', auth, createService);
+router.put('/:id', auth, updateService);
+router.delete('/:id', auth, deleteService);
+
+module.exports = router;
