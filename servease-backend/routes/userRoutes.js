45170760617b2cb2b2b7097ed52933f61a886162
Ebinezer Samuel <162179096+Ebinezersamuel@users.noreply.github.com>
2026-05-22 14:20:31 +0530

    Create user routes

---
+++ servease-backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { getUserProfile, updateUserProfile, searchUsers } = require('../controllers/userController');
+const { auth } = require('../middleware/auth');
+
+router.get('/profile', auth, getUserProfile);
+router.put('/profile', auth, updateUserProfile);
+router.get('/search', auth, searchUsers);
+
+module.exports = router;
